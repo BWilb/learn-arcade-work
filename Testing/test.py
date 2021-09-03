9af256173c6a839917dbb46cc6ff58fc602e5a68
Bryce Wilbur <bryce.wilbur@my.simpson.edu>
2021-09-02 20:37:17 -0500

    lab 1

---
+++ Testing/test.py
@@ -0,0 +1,10 @@
+print("Hello World")
+print("This is my first python course")
+print("i said " + "hi")
+
+print("i said \"Hi\"!")
+print("what does\nthe fox \nsay")
+print("""what am I doing you ask dear boy
+why I am 
+programming
+""")
